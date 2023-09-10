e9502de17583df259a5a9501e56945fb19063972
YuliiaTsaruk <julia.tsaruck@gmail.com>
2023-09-10 13:29:33 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+const markup = createMarkup(galleryItems);
+
+listEl.insertAdjacentHTML("beforeend", markup);
+listEl.addEventListener("click", onClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item" data-name="${description}">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+
+  const targetNameGalleryItem =
+    evt.target.closest(".gallery__item").dataset.name;
+
+  const dataGalleryItem = galleryItems.find(
+    (item) => item.description === targetNameGalleryItem
+  );
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+     <img
+      src="${dataGalleryItem.original}"
+      alt="${dataGalleryItem.description}"
+    />
+    </div>
+`);
+
+  instance.show();
+}
